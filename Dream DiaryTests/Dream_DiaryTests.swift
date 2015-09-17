8e24fc12821d4462e777c45221ea414e88462fd9
Andrew Scott <ascott@hmc.edu>
2015-09-17 10:21:25 -0700

    Initial Commit

---
+++ Dream DiaryTests/Dream_DiaryTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dream_DiaryTests.swift
+//  Dream DiaryTests
+//
+//  Created by Andrew Scott on 9/17/15.
+//
+//
+
+import XCTest
+@testable import Dream_Diary
+
+class Dream_DiaryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

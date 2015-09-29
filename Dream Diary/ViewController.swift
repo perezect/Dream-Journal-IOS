adc83305488675cb9c669539c4f19a5be0310506
Felis Perez <Perezect@Princess-Celestia.local>
2015-09-29 10:58:08 -0700

    Started the basic UI, added an ignore file

--- Dream Diary/ViewController.swift
+++ Dream Diary/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet weak var textBox: UITextView!
+    @IBOutlet weak var dreamNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textBox.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func textViewShouldReturn(textView: UITextView) -> Bool {
+        
+        textView.resignFirstResponder()
+        
+        return true
+    }
+    
+//    func textViewDidEndEditing(textView: UITextView) {
+//        
+//        dreamNameLabel.text = "Save your dream below"
+//    }
 
 
+//    @IBAction func saveButton(sender: UIButton) {
+//    }
 }
+
+
 

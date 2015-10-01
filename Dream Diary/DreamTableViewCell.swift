97cc5991ce2e08628bd0220a7931bbe462307166
Felis Perez <Perezect@Princess-Celestia.local>
2015-09-30 20:59:32 -0700

    Added table and cell data members.

---
+++ Dream Diary/DreamTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  DreamTableViewCell.swift
+//  Dream Diary
+//
+//  Created by Felis Perez on 9/30/15.
+//
+//
+
+import UIKit
+
+class DreamTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

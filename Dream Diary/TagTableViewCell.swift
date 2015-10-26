fcdd440dad9aba774b0b44ac2a4a6547624ae44a
Harry Cooke <HarryCooke@WL-26-160.CINE.Claremont.Edu>
2015-10-26 01:29:26 -0700

    got some of tags working

---
+++ Dream Diary/TagTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TagTableViewCell.swift
+//  Dream Diary
+//
+//  Created by Harry Cooke on 10/25/15.
+//
+//
+
+import UIKit
+
+class TagTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

acab6a316f3b2b9fb4afe655fb32a018c9c73d8a
Andrew Scott <ascott@hmc.edu>
2015-11-11 13:31:39 -0800

    created classes and a table view for the proper nouns page (will be very similar to the tag page)

---
+++ Dream Diary/ProperNounTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProperNounTableViewCell.swift
+//  Dream Diary
+//
+//  Created by Andrew Scott on 11/11/15.
+//
+//
+
+import UIKit
+
+class ProperNounTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
